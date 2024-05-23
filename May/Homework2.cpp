50f69010f0d494f5482a5dd810033ae5bdde89b2
Hessiie <hesmo2005@gmail.com>
2024-05-23 16:57:10 +0300

    Add files via upload

---
+++ May/Homework2.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+
+/*int main() {                                                       //Anul citit este bisect sau nu
+	int an;
+	cout << "Introduceti anul: ";
+	cin >> an;
+
+	if (an % 400 == 0 || (an % 4 == 0 && an % 100 != 0)) {
+		cout << an << " este bisect";
+	}
+	else
+	{
+		cout << an << " nu este bisect";
+	}
+
+}*/
+
+/*int main() {                                                       //5 factorial
+	int n = 1 * 2 * 3 * 4 * 5;
+	cout << n;
+
+}*/
+
+/*int main() {                                                       //Operatori logici && si ||
+	int a = 2;
+	int b = 4;
+
+	cout << ((a == 0) && (a > b)) << endl;
+	cout << ((a == 0) || (a < b)) << endl;
+}*/
+
+
+/*int main() {                                                       //Expresie aritmetica
+	float numar;
+	cout << "Introduceti un numar: ";
+	cin >> numar;
+
+	float a = numar += 5;
+	float b = numar -= 8;
+	float c = numar *= 3;
+	float d = numar /= 6;
+	cout << "Adunat cu numarul 5= " << a <<endl<< "Scazut cu numarul 8= " << b <<endl<< "Inmultit cu numarul 3= " << c<<endl<< "Impartit la numarul 6=" << d<<endl;
+}*/
+
+
+/*int main() {
+	float numar;
+	cout << "Introduceti un numar: ";
+	cin >> numar;
+
+	numar += 5;
+	cout << "Adunat cu numarul 5= " << numar << endl;
+	numar -= 10;
+	cout << "Scazut cu numarul 5=" << numar << endl;
+	(numar +=5) *= 5;
+	cout << "Inmultit cu numarul 5=" << numar << endl;
+	(numar /=5) /= 5;
+	cout << "Impartit cu numarul 5=" << numar << endl;
+}*/
---
+++ May/Homework3.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char c;
+    cout<<"Introduceti un caracter: ";
+    cin >> c;
+    if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+        cout << c << " este o vocala";
+    }
+    else {
+        cout << c << " nu este o vocala";
+    }
+}
+
+
+int main()
+{
+    int luna;
+    cout << "Introduceti o luna: ";
+    cin >> luna;
+    if (luna == 12 || luna <= 2 && luna > 0) {
+        cout << "Anotimpul este iarna";
+    }
+    else if (luna >= 3 && luna <= 5) {
+        cout << "Anotimpul este primavara";
+    }
+    else if (luna >= 6 && luna <= 8) {
+        cout << "Anotimpul este vara";
+    }
+    else if (luna >= 9 && luna <= 11) {
+        cout << "Anotimpul este toamna";
+    }
+    else cout << "Luna introdusa nu exista.";
+}
